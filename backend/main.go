a62a1125b368a570def5329714439b30194104f1
Jorge Celaya <jcelaya775@gmail.com>
2024-03-30 10:25:10 -0500

    Update configs

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome jjhes"))
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		write, err := w.Write([]byte("How are you?"))
+		if err != nil {
+			fmt.Println(write)
+		}
 	})
-	http.ListenAndServe(":3000", r)
+
+	fmt.Println("Listening on port 3000...")
+	err := http.ListenAndServe(":3000", router)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
