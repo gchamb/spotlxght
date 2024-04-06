4dbb5542ad4e08c50a3e5a174e7dcf17be04a861
Jorge Celaya <jcelaya775@gmail.com>
2024-04-05 22:17:33 -0500

    Update connection.go

--- backend/database/connection.go
+++ backend/database/connection.go
@@ -9,26 +9,20 @@ import (
 )
 
 func ConnectDB() (db *gorm.DB) {
-	dsn := "root:password@(mysql_db)/tbd?parseTime=true"
+	dsn := "root:example@(mysql_db)/tbd?parseTime=true" // TODO: get credentials from env
 	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN: dsn,
+		DSN:               dsn,
 		DefaultStringSize: 255,
 	}), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
-		
 	})
-
 	if err != nil {
-		panic("failed to connect database")
+		panic("Failed to connect to database.")
 	}
 
-	// create tables if not created 
-	err = db.AutoMigrate(&models.User{}, &models.Song{}, &models.Tag{})
-
-	if err != nil {
-		panic("failed to auto migrate")
+	if err := db.AutoMigrate(&models.User{}, &models.Song{}, &models.Tag{}); err != nil {
+		panic("Failed to auto migrate.")
 	}
-
 
 	return db
 }
