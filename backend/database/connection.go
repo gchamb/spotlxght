0eb3fa18cf686631a9365a59ea9f30d38b21d8b7
gchamb <gchamb.dev@gmail.com>
2024-04-07 08:08:02 -0500

    wip: let connection string come from env and past methods here

--- backend/database/connection.go
+++ backend/database/connection.go
@@ -2,14 +2,23 @@ package database
 
 import (
 	"backend/models"
+	// "encoding/json"
+	// "fmt"
+
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
-func ConnectDB() (db *gorm.DB) {
-	dsn := "root:example@(mysql_db)/tbd?parseTime=true" // TODO: get credentials from env
+
+func ConnectDB() (*gorm.DB) {
+	var dsn string
+	if dsn = os.Getenv("MYSQL_CONNECTION_STRING"); dsn == "" {
+		panic("MySQL connection string hasn't been provided.")
+	}
+	
 	db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN:               dsn,
 		DefaultStringSize: 255,
@@ -26,3 +35,63 @@ func ConnectDB() (db *gorm.DB) {
 
 	return db
 }
+
+
+// func (db *gorm.DB) CreateUser(user models.User) {
+// 	db.Create(&user)
+// }
+
+// func (db *gorm.DB) AddFollower(userID, followerID string) {
+// 	var followed models.User
+// 	if err := db.First(&followed, "id = ?", userID).Error; err != nil {
+// 		panic("User being followed not found")
+// 	}
+
+// 	var following models.User
+// 	if err := db.First(&following, "id = ?", followerID).Error; err != nil {
+// 		panic("Following user not found")
+// 	}
+
+// 	if err := db.Model(&followed).Association("Followers").Append(&following); err != nil {
+// 		panic("Error adding follower to user")
+// 	}
+// 	fmt.Printf("Added follower %v to user %v\n", followerID, userID)
+// }
+
+// func (db *gorm.DB) AddFollowers(userID string, followerIDs ...string) {
+// 	var followed models.User
+// 	if err := db.First(&followed, "id = ?", userID).Error; err != nil {
+// 		panic("User being followed not found")
+// 	}
+
+// 	followers := make([]models.User, len(followerIDs))
+// 	for i, followerID := range followerIDs {
+// 		var follower models.User
+// 		if err := db.First(&follower, "id = ?", followerID).Error; err != nil {
+// 			panic("Following user not found: " + followerID)
+// 		}
+// 		followers[i] = follower
+// 	}
+
+// 	if err := db.Model(&followed).Association("Followers").Append(followers); err != nil {
+// 		panic("Error adding followers to user")
+// 	}
+// 	fmt.Printf("Added followers %v to user %v\n", followerIDs, userID)
+// }
+
+// func (db *gorm.DB) PrintFollowers(userID string) {
+// 	var user models.User
+// 	db.First(&user, "id = ?", userID)
+// 	var userFollowers []models.User
+// 	db.Model(&user).Association("Followers").Find(&userFollowers)
+
+// 	followerBytes, err := json.MarshalIndent(userFollowers, "", "\t")
+// 	if err != nil {
+// 		panic("Error marshalling userFollowers")
+// 	}
+// 	fmt.Printf("User's followers: %v\n", string(followerBytes))
+// }
+
+// func (db *gorm.DB) CreateSong(song models.Song) {
+// 	db.Create(&song)
+// }
