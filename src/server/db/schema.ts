5724f451877440f83bea2060bbffcd0eae4ca6ed
gchamb <gchamb.dev@gmail.com>
2024-06-08 16:50:10 -0500

    add data model to drizzle schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -19,45 +19,30 @@ import { type AdapterAccount } from "next-auth/adapters";
  */
 export const createTable = mysqlTableCreator((name) => `underground_${name}`);
 
-export const posts = createTable(
-  "post",
-  {
-    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    name: varchar("name", { length: 256 }),
-    createdById: varchar("createdById", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt").onUpdateNow(),
-  },
-  (example) => ({
-    createdByIdIdx: index("createdById_idx").on(example.createdById),
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
-
 export const users = createTable("user", {
-  id: varchar("id", { length: 255 }).notNull().primaryKey(),
+  id: varchar("id", { length: 191 }).notNull().primaryKey(),
   name: varchar("name", { length: 255 }),
-  email: varchar("email", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).unique().notNull(),
   emailVerified: timestamp("emailVerified", {
     mode: "date",
     fsp: 3,
   }).default(sql`CURRENT_TIMESTAMP(3)`),
+  password: varchar("password", { length: 255 }),
   image: varchar("image", { length: 255 }),
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
   accounts: many(accounts),
   sessions: many(sessions),
+  reviews: many(reviews),
+  assets: many(assets),
+  applications: many(applications),
 }));
 
 export const accounts = createTable(
   "account",
   {
-    userId: varchar("userId", { length: 255 })
+    userId: varchar("userId", { length: 191 })
       .notNull()
       .references(() => users.id),
     type: varchar("type", { length: 255 })
@@ -78,7 +63,7 @@ export const accounts = createTable(
       columns: [account.provider, account.providerAccountId],
     }),
     userIdIdx: index("account_userId_idx").on(account.userId),
-  })
+  }),
 );
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
@@ -98,21 +83,126 @@ export const sessions = createTable(
   },
   (session) => ({
     userIdIdx: index("session_userId_idx").on(session.userId),
-  })
+  }),
 );
 
 export const sessionsRelations = relations(sessions, ({ one }) => ({
   user: one(users, { fields: [sessions.userId], references: [users.id] }),
 }));
 
-export const verificationTokens = createTable(
-  "verificationToken",
+export const reviews = createTable(
+  "review",
   {
-    identifier: varchar("identifier", { length: 255 }).notNull(),
-    token: varchar("token", { length: 255 }).notNull(),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
+    id: varchar("id", { length: 191 }).notNull().primaryKey(),
+    reviewer: varchar("id", { length: 191 })
+      .notNull()
+      .references(() => users.id),
+    message: varchar("message", { length: 255 }),
+    reviewedAt: timestamp("reviewedAt", { mode: "date" }).notNull(),
+    rate: int("rate").notNull(),
+    userId: varchar("userId", { length: 191 })
+      .notNull()
+      .references(() => users.id),
   },
-  (vt) => ({
-    compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  })
+  (review) => ({
+    userIdIdx: index("reviews_userId_idx").on(review.userId),
+  }),
 );
+
+export const reviewsRelations = relations(reviews, ({ one }) => ({
+  user: one(users, { fields: [reviews.userId], references: [users.id] }),
+}));
+
+export const assets = createTable(
+  "asset",
+  {
+    id: varchar("id", { length: 191 }).notNull().primaryKey(),
+    type: varchar("type", { length: 25 }).notNull(),
+    mimetype: varchar("mimetype", { length: 25 }).notNull(),
+    azureBlobKey: varchar("azureBlobKey", { length: 191 }).notNull(),
+    uploadedAt: timestamp("uploadedAt", { mode: "date" }).notNull(),
+    userId: varchar("userId", { length: 191 })
+      .notNull()
+      .references(() => users.id),
+  },
+  (asset) => ({
+    userIdIdx: index("assets_userId_idx").on(asset.userId),
+  }),
+);
+
+export const assetsRelations = relations(assets, ({ one }) => ({
+  user: one(users, { fields: [assets.userId], references: [users.id] }),
+}));
+
+export const events = createTable(
+  "event",
+  {
+    id: varchar("id", { length: 191 }).notNull().primaryKey(),
+    name: varchar("name", { length: 50 }).notNull(),
+    status: int("mimetype").notNull(),
+    createdAt: timestamp("createdAt", { mode: "date" }).notNull(),
+    venueId: varchar("venueId", { length: 191 })
+      .notNull()
+      .references(() => users.id),
+  },
+  (event) => ({
+    userIdIdx: index("events_userId_idx").on(event.venueId),
+  }),
+);
+
+export const eventsRelations = relations(events, ({ one, many }) => ({
+  user: one(users, { fields: [events.venueId], references: [users.id] }),
+  timeslots: many(timeslots),
+  applications: many(applications),
+}));
+
+export const timeslots = createTable(
+  "timeslot",
+  {
+    id: varchar("id", { length: 191 }).notNull().primaryKey(),
+    startDate: timestamp("startDate", { mode: "date" }).notNull(),
+    endDate: timestamp("endDate", { mode: "date" }).notNull(),
+    timezone: varchar("timezone", { length: 5 }).default("CST").notNull(),
+    eventId: varchar("eventId", { length: 191 })
+      .notNull()
+      .references(() => events.id),
+  },
+  (timeslot) => ({
+    eventIdIdx: index("events_userId_idx").on(timeslot.eventId),
+  }),
+);
+
+export const timeslotsRelations = relations(timeslots, ({ one }) => ({
+  event: one(events, { fields: [timeslots.eventId], references: [events.id] }),
+}));
+
+export const applications = createTable(
+  "application",
+  {
+    id: varchar("id", { length: 191 }).notNull().primaryKey(),
+    timeslotId: varchar("timeslotId", { length: 191 }).notNull(),
+    eventId: varchar("eventId", { length: 191 }).notNull(),
+    userId: varchar("userId", { length: 191 }).notNull(),
+    status: int("status").notNull(),
+    appliedAt: timestamp("endDate", { mode: "date" }).notNull(),
+  },
+  (application) => ({
+    userIdIdx: index("application_userId_idx").on(application.userId),
+    eventIdIdx: index("application_eventId_idx").on(application.eventId),
+    timeslotIdIdx: index("application_timeslotId_idx").on(
+      application.timeslotId,
+    ),
+  }),
+);
+
+export const applicantsRelations = relations(applications, ({ one, many }) => ({
+  timeslot: one(timeslots, {
+    fields: [applications.timeslotId],
+    references: [timeslots.id],
+  }),
+  user: one(users, { fields: [applications.userId], references: [users.id] }),
+  event: one(events, {
+    fields: [applications.eventId],
+    references: [events.id],
+  }),
+}));
