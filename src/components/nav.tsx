91a862c71d2fd9b5104db078f5b260f2ce001eb4
gchamb <gchamb.dev@gmail.com>
2024-06-10 21:50:16 -0500

    feat: :sparkles: created nav bar to match mockups

---
+++ src/components/nav.tsx
@@ -0,0 +1,13 @@
+export default function Nav() {
+  // NOTE: appropriately render based on user type (venue or musician)
+  return (
+    <nav className="w-full max-w-screen-2xl mx-auto p-4">
+      <div>
+        <a>
+          <h1 className="text-xl font-semibold">underground</h1>
+        </a>
+      </div>
+      <div></div>
+    </nav>
+  );
+}
