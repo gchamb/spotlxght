4061a57c636b731586a33f8477c3c51336beb52f
gchamb <gchamb.dev@gmail.com>
2024-06-15 21:31:17 -0500

    wip: conditionally render nav based on user type

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,11 +1,22 @@
-export default function Nav() {
-  // NOTE: appropriately render based on user type (venue or musician)
+import { auth } from "~/auth";
+
+export default async function Nav() {
+  const session = await auth();
+
+  console.log(session);
+
   return (
-    <nav className="w-full max-w-screen-2xl mx-auto p-4">
-      <div>
-        <a>
+    <nav className="mx-auto w-full max-w-screen-2xl p-4">
+      <div className="flex items-center gap-x-24">
+        <a href="/">
           <h1 className="text-xl font-semibold">underground</h1>
         </a>
+        <div>
+          {session?.user.type === "venue" && <a href="/my-events">My Events</a>}
+          {session?.user.type === "musician" && (
+            <a href="/listings">My Events</a>
+          )}
+        </div>
       </div>
       <div></div>
     </nav>
