49b2a4133fb6e2849dbfb9b3df3e26de7bea03da
gchamb <gchamb.dev@gmail.com>
2024-06-20 09:48:47 -0500

    style: :lipstick: made it from row into a card

--- src/app/events/components/applicant-row.tsx
+++ src/app/events/components/applicant-row.tsx
@@ -7,59 +7,80 @@ import Link from "next/link";
 import { useState } from "react";
 import AssetsDialog from "./assets-dialog";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+
 type ApplicantRowProps = {
   applicant: TimeslotProps["applicants"][number];
 };
 
-export default function ApplicantRow({ applicant }: ApplicantRowProps) {
+export default function ApplicantCard({ applicant }: ApplicantRowProps) {
   const [viewPerformances, setViewPerformances] = useState(false);
 
   return (
-    <div className="relative grid grid-rows-5 items-center rounded-2xl bg-underground-dark-grey p-4 drop-shadow-lg lg:grid-cols-5  lg:grid-rows-none lg:gap-4">
-      <div className="flex flex-col items-center gap-1">
-        <div className="mx-auto h-[75px] w-[75px] lg:m-0">
-          <img
-            className="rounded w-full h-full object-cover"
-            src={applicant.profilePicImage ?? "/images/default-profile2.png"}
-          />
+    <Card>
+      <CardHeader>
+        <div className="flex flex-col items-center gap-1">
+          <div className="mx-auto h-[75px] w-[75px] lg:m-0">
+            <img
+              className="h-full w-full rounded object-cover"
+              src={applicant.profilePicImage ?? "/images/default-profile2.png"}
+            />
+          </div>
+
+          <span className="text-center">{applicant.name}</span>
         </div>
-
-        <span className="text-center">{applicant.name}</span>
-      </div>
-
-      {/* will add later */}
-      {/* <div className="flex justify-center">
+      </CardHeader>
+      <CardContent>
+        <div className="relative grid grid-rows-3 gap-y-2 items-center rounded-2xl">
+          {/* will add later */}
+          {/* <div className="flex justify-center">
           <Ratings rating={3} />
         </div> */}
 
-      <div className="flex justify-center">
-        <Chip
-          text={applicant.applicantData?.status ?? ""}
-          color={
-            applicant.applicantData?.status === "requested"
-              ? "gray"
-              : applicant.applicantData?.status === "accepted"
-                ? "green"
-                : applicant.applicantData?.status === "rejected"
-                  ? "red"
-                  : "gray"
-          }
-        />
-      </div>
-      <span className="text-center">
-        {applicant.applicantData?.appliedAt.toISOString()}
-      </span>
-      {applicant.applicantData?.status === "requested" && (
-        <div className="flex justify-center gap-x-2">
-          <Button variant="ghost">
-            <Check className="text-green-500" />
-          </Button>
-          <Button variant="ghost">
-            <X className="text-red-500" />
-          </Button>
+          <div className="flex flex-col items-center justify-center gap-1">
+            <span className="text-lg font-semibold">Status</span>
+            <Chip
+              text={applicant.applicantData?.status ?? ""}
+              color={
+                applicant.applicantData?.status === "requested"
+                  ? "gray"
+                  : applicant.applicantData?.status === "accepted"
+                    ? "green"
+                    : applicant.applicantData?.status === "rejected"
+                      ? "red"
+                      : "gray"
+              }
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center gap-1">
+            <span className="text-lg font-semibold">Applied At</span>
+            <span className="text-center">
+              {applicant.applicantData?.appliedAt.toISOString()}
+            </span>
+          </div>
+          {applicant.applicantData?.status === "requested" && (
+            <div className="flex flex-col items-center justify-center gap-1">
+              <span className="text-center text-lg font-semibold">Options</span>
+              <div className="flex justify-center gap-x-2">
+                <Button variant="ghost">
+                  <Check className="text-green-500" />
+                </Button>
+                <Button variant="ghost">
+                  <X className="text-red-500" />
+                </Button>
+              </div>
+            </div>
+          )}
         </div>
-      )}
-      <div className="absolute bottom-2 left-0 right-0  flex justify-center gap-x-2 lg:right-2 lg:justify-end ">
+      </CardContent>
+      <CardFooter className="justify-center">
         <Button variant="ghost">
           <Link
             className="flex items-center gap-x-2"
@@ -70,7 +91,7 @@ export default function ApplicantRow({ applicant }: ApplicantRowProps) {
           </Link>
         </Button>
         <AssetsDialog name={applicant.name ?? ""} assets={applicant.assets} />
-      </div>
-    </div>
+      </CardFooter>
+    </Card>
   );
 }
