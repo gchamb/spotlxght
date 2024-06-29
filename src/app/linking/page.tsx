96ba37c8013ccbe29692e0df645926e0d365dc54
gchamb <gianttechambers@gmail.com>
2024-06-29 16:32:16 -0500

    feat: :sparkles: Change page title match page

--- src/app/linking/page.tsx
+++ src/app/linking/page.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import LinkStripe from "~/components/link-stripe";
 import { getSession } from "~/lib/auth";
+
+export const metadata: Metadata = {
+  title: "Link Your Account",
+};
 
 export default async function StripeLinking() {
   const session = await getSession();
--- src/app/profile/[userId]/page.tsx
+++ src/app/profile/[userId]/page.tsx
@@ -1,4 +1,8 @@
+import { eq } from "drizzle-orm";
+import { ResolvingMetadata, Metadata } from "next";
 import Profile from "~/app/profile/components/profile";
+import { db } from "~/server/db";
+import { users } from "~/server/db/schema";
 
 export default async function ProfilePage({
   params,
@@ -6,4 +10,24 @@ export default async function ProfilePage({
   params: { userId: string };
 }) {
   return <Profile userId={params.userId} />;
+}
+
+export async function generateMetadata(
+  {
+    params,
+  }: {
+    params: { userId: string };
+  },
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const id = params.userId;
+
+  const user = await db.query.users.findFirst({
+    where: eq(users.id, id),
+    columns: { name: true },
+  });
+
+  return {
+    title: `${user?.name ?? "Spotlight"} Profile`,
+  };
 }
